f95e64dc2eb759fc53e041a9a37ba597c978cb44
similadayo <similoluwailesanmi@outlook.com>
2023-03-10 07:30:59 +0100

    Job model

--- app/Models/Jobs.php
+++ app/Models/Jobs.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Jobs extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "user_id",
+        "title",
+        "description",
+        "location",
+        "salary",
+        "type",
+        "category",
+        "company",
+        "category",
+        "is_active"
+    ];
+
+    public function user()
+    {
+       return $this->belongsTo(User::class);
+    }
 }
